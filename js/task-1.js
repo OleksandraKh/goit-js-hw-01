52db59ab06474d7f6f55605045fe8de57dc14699
Oleksandra Khlebynska <akhlebinskaya@gmail.com>
2020-08-11 20:51:49 +0300

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+// const name = '"Генератор защитного поля"';
+// const price = 1000;
+// const priceNew = 2000;
+
+// console.log('Выбран', name, 'цена за штуку',price, 'кредитов' );
+// console.log('Выбран "',name,'"', ',цена за штуку',priceNew, 'кредитов' );
+
+
+const name = '«Генератор защитного поля»'; // Write code on this line
+const price = 1000; // Write code on this line
+ const firstMessage ='Выбран', name 'цена за штуку', price, 'кредитов'; // Write code on this line
+//price = 2000;
+//const secondMessage = ``; // Write code on this line
+
+console.log(firstMessage);
